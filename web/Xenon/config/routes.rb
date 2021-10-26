a472157da5a0d71cc244f0954bd561eb6a4ab688
Phytolizer <kyle.coffey@tamu.edu>
2021-10-26 00:23:09 -0500

    Add RuboCop and plugins

--- web/Xenon/config/routes.rb
+++ web/Xenon/config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
+# frozen_string_literal: true
 
-  root 'welcome#index'
+Rails.application.routes.draw do
+  get "welcome/index"
+
+  root "welcome#index"
 end
